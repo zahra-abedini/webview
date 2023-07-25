34a7a720934710d2fc8241df1f5f4cd90a8769d1
Zahra Abedini <z.abedini1998@gmail.com>
2023-07-25 12:20:45 +0330

    Edit

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
+  const indexHTML = require('./index.html');
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <WebView
+      style={styles.container}
+      originWhitelist={['*']}
+      source={indexHTML}
+    />
   );
 }
 
